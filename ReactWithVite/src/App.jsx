38d08b8241e1681aa039fce2af32f9797e3e35a0
ShubhamSharma1309 <itsmeshubhamsharma1106@gmail.com>
2024-08-11 12:01:43 +0530

    CounterApp

--- ReactWithVite/src/App.jsx
+++ ReactWithVite/src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
+
 
 function App() {
+
+  const [counter , setCounter] = useState(0);  // useState return karta hai ek variable and ek function,, and jo v pass karte hai usestate mai vo variable mai set hojata hai...
+
+  const addNumber = () => {
+    if(counter<20){
+      setCounter(counter+1);
+    }
+  }
+  const deleteNumber = () => {
+    if(counter>-20){
+      setCounter(counter-1);
+    }
+  }
+  const resetNumber = () => {
+    setCounter(0);
+  }
   return (
-    <h1>Hello My Name is Shubham Sharma</h1>
+    <div>
+      <h1>click on ADD to increase and Substract to decrease</h1>
+      <h2>Counter Value : {counter}</h2>
+      <div>
+        <button onClick={addNumber}>Add</button>
+      </div>
+      <div>
+        <button onClick={deleteNumber}>Subtract</button>
+      </div>
+      <div>
+        <button onClick={resetNumber}>Reset</button>
+      </div>
+    </div>
   )
 }
 
